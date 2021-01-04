841684d2f9187488f11b249a58677d47a14bb50b
rockfeng0 <lkf20031988@163.com>
2021-01-04 10:58:52 +0800

    delete old flask_demo

--- flask_demo/demo/views/simple/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-simple = Blueprint('simple', __name__)
-
-from . import views
--- flask_demo/demo/views/sql/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-sql = Blueprint('sql', __name__)
-from . import views
