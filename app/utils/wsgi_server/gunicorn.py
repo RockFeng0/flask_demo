e8b29b0d6cb4ea3070ff0acd6ad5141ba8f13ef3
rockfeng0 <lkf20031988@163.com>
2021-01-06 10:28:43 +0800

    添加utils.wsgi_server模块，支持gunicorn

--- app/utils/wsgi_server/gunicorn.py
+++ app/utils/wsgi_server/gunicorn.py
@@ -1,10 +1,26 @@
 #! python3
 # -*- encoding: utf-8 -*-
-
+import os
+import multiprocessing
 from abc import ABC
 
 from gunicorn.six import iteritems
 from gunicorn.app.base import BaseApplication
+from app.config import ROOT_PATH
+
+
+class GunicornConfig(object):
+    """ gunicorn配置(Linux生产环境支持gunicorn) """
+    bind = '127.0.0.1:5000'
+    workers = multiprocessing.cpu_count() * 2 + 1
+    worker_connections = 10000
+    timeout = 60
+    log_level = 'INFO'
+    log_dir_path = os.path.join(ROOT_PATH, 'logs')
+    log_file_max_bytes = 1024 * 1024 * 100
+    log_file_backup_count = 10
+    backlog = 64
+    pid_file = 'run.pid'
 
 
 class StandaloneApplication(BaseApplication, ABC):
